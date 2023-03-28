64e4bb2753f698635129a4b63fe359ee6091e5f7
Noah Gearhart <excelsiorvfx@gmail.com>
2023-03-27 21:41:42 -0400

    Initial commit

---
+++ db.py
@@ -0,0 +1,25 @@
+from firebase import firebase
+from dotenv import load_dotenv
+from os import environ
+import json
+from functools import cache
+
+load_dotenv()
+
+## Verify environment
+ENV_VAR_NAMES = {
+    'config': 'FIREBASE_URL',
+    'username': 'FIREBASE_USERNAME',
+    'password': 'FIREBASE_PASSWORD'
+}
+
+@cache
+def get_firebase():
+    for key in ENV_VAR_NAMES:
+        if ENV_VAR_NAMES[key] not in environ:
+            raise Exception(f'{ENV_VAR_NAMES[key]} missing from environment variables')
+
+    auth = firebase.FirebaseAuthentication(environ.get(ENV_VAR_NAMES['password']), email=environ.get(ENV_VAR_NAMES['username']))
+    url = environ.get(ENV_VAR_NAMES['config'])
+
+    return firebase.FirebaseApplication(url, auth)
