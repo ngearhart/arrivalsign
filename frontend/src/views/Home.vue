d7b0a292f52cc3cd3331ee66975b2a72adf70768
Noah Gearhart <excelsiorvfx@gmail.com>
2023-04-04 21:38:06 -0400

    Dev

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,45 @@
 <template>
   <base-layout>
-    <div>Hi</div>
+    <v-card title="Widgets">
+      <template v-slot:text>
+        <v-data-table
+          :headers="headers"
+          :items="activeWidgets"
+          item-value="name"
+          class="elevation-1"
+        ></v-data-table>
+      </template>
+    </v-card>
   </base-layout>
 </template>
 
 <script lang="ts" setup>
 import BaseLayout from '@/components/BaseLayout.vue'
+import { onMounted, ref, reactive } from 'vue'
+import { useDatabaseList  } from 'vuefire'
+import { ref as dbRef, getDatabase } from 'firebase/database'
+
+const activeWidgets = useDatabaseList<GenericWidget>(dbRef(getDatabase(), 'widgets'))
+
+const defaultTrain: DCMetroTrainArrivalWidget = {
+  custom_trains: [],
+  name: "DCMetroTrainArrivalWidget",
+  station_id: "K07",
+  enabled: true
+}
+
+
+// activeWidgets.value.push(defaultTrain)
+
+const headers = reactive([
+  {
+    title: 'Widget Name',
+    key: 'name',
+  },
+  { title: 'Configure', sortable: false },
+])
+
+onMounted(() => {
+})
+
 </script>
