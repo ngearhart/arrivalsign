0c2aed26e7b5950976bcbddfd81af9f7241903d1
Noah Gearhart <excelsiorvfx@gmail.com>
2023-04-23 15:11:36 -0400

    Not none

--- admin/main.py
+++ admin/main.py
@@ -6,7 +6,7 @@ import os
 default_app = firebase_admin.initialize_app()
 
 def set_admin_by_email_or_uid(admin: bool, **kwargs):
-    if 'email' in kwargs:
+    if 'email' in kwargs and kwargs['email'] is not None:
         try: 
             user: auth.UserRecord = auth.get_user_by_email(kwargs['email'], default_app)
         except _auth_utils.UserNotFoundError:
@@ -16,7 +16,7 @@ def set_admin_by_email_or_uid(admin: bool, **kwargs):
                 print(f' - {user.email}: {user.uid}')
             raise
         set_admin(user.uid, admin)
-    elif 'uid' in kwargs:
+    elif 'uid' in kwargs and kwargs['uid'] is not None:
         set_admin(kwargs['uid'], admin)
     else:
         raise Exception('Invalid arguments: need email or uid')
