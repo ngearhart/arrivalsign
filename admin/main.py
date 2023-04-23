036e53436131d444440b6852239410ae1826f077
Noah Gearhart <excelsiorvfx@gmail.com>
2023-04-23 12:45:12 -0400

    Add self pull

--- admin/main.py
+++ admin/main.py
@@ -1,7 +1,17 @@
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import auth
 
-cred = credentials.Certificate('path/to/serviceAccountKey.json')
+import os
+
 default_app = firebase_admin.initialize_app()
 
+def set_admin(email: str, admin: bool):
+    user: auth.UserRecord = auth.get_user_by_email(email, default_app)
+    auth.set_custom_user_claims(user.uid, {'admin': admin})
+    print('done')
 
+def main():
+    set_admin(os.environ['EMAIL'], os.environ['ADMIN'] == 'True')
+
+if __name__ == '__main__':
+    main()
