67cc5550b33a3b5565b9ced2c2bcfaa57c19364e
Noah Gearhart <excelsiorvfx@gmail.com>
2023-04-23 14:05:35 -0400

    Change set method

--- admin/main.py
+++ admin/main.py
@@ -1,19 +1,33 @@
 import firebase_admin
-from firebase_admin import auth
+from firebase_admin import auth, _auth_utils
 
 import os
 
 default_app = firebase_admin.initialize_app()
 
-def set_admin(email: str, admin: bool):
-    user: auth.UserRecord = auth.get_user_by_email(email, default_app)
-    auth.set_custom_user_claims(user.uid, {'admin': admin})
+def set_admin_by_email_or_uid(admin: bool, **kwargs):
+    if 'email' in kwargs:
+        try: 
+            user: auth.UserRecord = auth.get_user_by_email(kwargs['email'], default_app)
+        except _auth_utils.UserNotFoundError:
+            print('User not found.')
+            for user in auth.get_users(default_app):
+                print(f' - {user.email}: {user.uid}')
+            raise
+        set_admin(user.uid, admin)
+    elif 'uid' in kwargs:
+        set_admin(kwargs['uid'], admin)
+    else:
+        raise Exception('Invalid arguments: need email or uid')
+
+def set_admin(uid: str, admin: bool):
+    auth.set_custom_user_claims(uid, {'admin': admin})
     print('done')
 
 def main():
     print('Running user admin task')
     print(f'Using app {default_app.name} ({default_app.project_id})')
-    set_admin(os.environ['EMAIL'], os.environ['ADMIN'] == 'True')
+    set_admin(os.environ.get('ADMIN') == 'True', email=os.environ.get('EMAIL'), uid=os.environ.get('UID'))
 
 if __name__ == '__main__':
     main()
