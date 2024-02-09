c081e1ce360e1b3fd0dee223e96d091dc30b68c5
Noah Gearhart <excelsiorvfx@gmail.com>
2024-02-08 21:50:04 -0500

    Final changes

--- pi/main.py
+++ pi/main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import dotenv
 dotenv.load_dotenv()
 
--- pi/run.sh
+++ pi/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-until main.py; do
+runscript(){
+    cd /root/arrivalsign/pi
+    python3 main.py
+}
+
+until runscript; do
     echo "'main.py' crashed with exit code $?. Restarting..." >&2
     sleep 1
 done
